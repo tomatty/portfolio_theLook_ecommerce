74089a89c73f3e4d7ab834b2d28fb9784aea72fa
takuya <81954921+tomatty@users.noreply.github.com>
2025-01-10 21:23:34 +0900

    Update basic_view_sales.sql

--- basic_view_sales.sql
+++ basic_view_sales.sql
@@ -5,6 +5,18 @@ SELECT PARSE_DATE("%Y-%m-%d",FORMAT_TIMESTAMP("%Y-%m-%d",order_items.created_at)
       ,order_items.order_id
       ,order_items.user_id
       ,users.age
+      ,CASE
+        WHEN age BETWEEN 10 AND 19 THEN "10代"
+        WHEN age BETWEEN 20 AND 29 THEN "20代"
+        WHEN age BETWEEN 30 AND 39 THEN "30代"
+        WHEN age BETWEEN 40 AND 49 THEN "40代"
+        WHEN age BETWEEN 50 AND 59 THEN "50代"
+        WHEN age BETWEEN 60 AND 69 THEN "60代"
+        WHEN age BETWEEN 70 AND 79 THEN "70代"
+        WHEN age BETWEEN 80 AND 89 THEN "80代"
+        WHEN age BETWEEN 90 AND 99 THEN "90代"
+        ELSE "その他"
+        END generation
       ,users.gender
       ,users.state
       ,users.country
